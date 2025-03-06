10bd567942d1a3f15199fd8ca38285fe2d6593d8
spDev <sp.xayyalath@gmail.com>
2025-03-06 21:47:48 +0700

    [ADD] using map

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ const SelfIntroddution = ({name = 'unknown',dateOfBirth, hobbies =[]}) => {
   <div>
   <h1>My name is : {name} </h1>
   {dateOfBirth ? <h1> i was born on {dateOfBirth} </h1> : null}
-  {hobbies.length === 0 ? null : <h1>My hobbies are: {hobbies.join(', ')}</h1>}
+  {hobbies.length === 0 ? null : 
+  <div>
+  <h1>My hobbies are : </h1>
+  <ul>
+  {hobbies.map((item, index) => <div key={item}>  {index+1}. {item}</div>)}
+  </ul>
+  </div>
+  
+  }
   
   </div>)
 }
 function App() {
   return (
    <div className="App">
-    <SelfIntroddution name="somphone" dateOfBirth="2000 jun 26"/>
+    <SelfIntroddution name="somphone" dateOfBirth="2000 jun 26" hobbies={['coding', 'playing', 'sleeping']}/>
     </div>
   );
 }
