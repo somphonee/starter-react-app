53d443a4507c870d823579fa79f013660d3b38e8
spDev <sp.xayyalath@gmail.com>
2025-03-25 23:47:25 +0700

    [ADD] enhance SelfIntroduction component with hobbies input management and improve formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,47 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import PropTypes from "prop-types";
 
 import "dayjs/locale/lo";
-import dayjs from 'dayjs';
-import bhuddishtEra from 'dayjs/plugin/buddhistEra';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
+import dayjs from "dayjs";
+import bhuddishtEra from "dayjs/plugin/buddhistEra";
+import localizedFormat from "dayjs/plugin/localizedFormat";
 
-
-dayjs.locale('lo');
+dayjs.locale("lo");
 dayjs.extend(localizedFormat);
 dayjs.extend(bhuddishtEra);
 
-
-
-
-
-
-const SelfIntroddution = ({ name = 'unknown', dateOfBirth, hobbies = [] }) => {
+const SelfIntroddution = ({ name = "unknown", dateOfBirth, hobbies = [] }) => {
   return (
     <div>
-      <h1>My name is : <span style={{ color: 'red' }}>{name}</span> </h1>
-      {dateOfBirth ? <h1> i was born on {dayjs(dateOfBirth).format("D MMMM YYYY")} </h1> : null}
-      {hobbies.length === 0 ? null :
+      <h1>
+        My name is : <span style={{ color: "red" }}>{name}</span>{" "}
+      </h1>
+      {dateOfBirth ? (
+        <h1> i was born on {dayjs(dateOfBirth).format("D MMMM YYYY")} </h1>
+      ) : null}
+      {hobbies.length === 0 ? null : (
         <div>
           <h1>My hobbies are : </h1>
           <ul>
-            {hobbies.map((item, index) => <div key={item}>  {index + 1}. {item}</div>)}
+            {hobbies.map((item, index) => (
+              <div key={item}>
+                {" "}
+                {index + 1}. {item}
+              </div>
+            ))}
           </ul>
         </div>
-
-      }
-
-    </div>);
+      )}
+    </div>
+  );
 };
 // <SelfIntroddution}
 
 SelfIntroddution.prototype = {
   name: PropTypes.string.isRequired,
   dateOfBirth: PropTypes.string,
-  hobbies: PropTypes.array
+  hobbies: PropTypes.array,
 };
 const Test = ({ children }) => {
   console.log(typeof children);
@@ -49,12 +51,49 @@ function App() {
   const [data, setData] = useState({ name: undefined, dateOfBirth: undefined });
   const [name, setName] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
+  const [hobbies, setHobbies] = useState([""]);
+  console.log(hobbies);
   return (
     <div className="App">
       <label htmlFor="name">Name:</label>
-      <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        id="name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label htmlFor="Date Of Birth">Date Of Birth:</label>
-      <input id="Date Of Birth" type="date" pattern="mm/dd/yyyy" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} />
+      <input
+        id="Date Of Birth"
+        type="date"
+        pattern="mm/dd/yyyy"
+        value={dateOfBirth}
+        onChange={(e) => setDateOfBirth(e.target.value)}
+      />
+      <div>
+        {hobbies.map((item, index) => (
+          <div key={index}>
+            <label htmlFor="hobbies">Hobbies: {index + 1}: </label>
+            <input
+              id={`hobby-${index}`}
+              type="text"
+              value={item} onChange={e => {
+                const newValue = e.target.value;
+                // setHobbies(hobbies.map((hobby, hobbyIndex) => {
+                //   if (index === hobbyIndex)  {
+                //     return newValue;
+                //   }
+                //   return hobby;
+                // }));
+
+                const newHobbies = setHobbies(hobbies.map((hobby, hobbyIndex) => hobby === index ? newValue : hobby));
+                setHobbies(newHobbies);
+              }}
+            />
+          </div>
+        ))}
+        <button onClick={() => setHobbies([...hobbies, ""])}>Add anothor hobby</button>
+      </div>
 
       <button
         onClick={() => {
@@ -63,12 +102,13 @@ function App() {
           setDateOfBirth("");
         }}
       >
-        Save</button>
+        Save
+      </button>
 
       <SelfIntroddution
         name={data.name}
         dateOfBirth={data.dateOfBirth}
-        hobbies={['coding', 'playing', 'sleeping']}
+        hobbies={["coding", "playing", "sleeping"]}
       />
       <Test>123</Test>
     </div>
